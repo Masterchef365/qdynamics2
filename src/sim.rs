de208049e6689ee66e719a7478c1b1fddb5fc3fc
Duncan Freeman <duncan.freeman1@gmail.com>
2024-01-21 14:00:22 -0800

    Okay that's better

--- src/sim.rs
+++ src/sim.rs
@@ -157,7 +157,7 @@ fn grid_positions(cfg: &SimConfig) -> Array2D<Point2<f64>> {
 
 /// Returns false if out of bounds with the given width
 fn bounds_check(pt: Point2<i32>, width: i32) -> Option<(usize, usize)> {
-    (pt.x > 0 && pt.y > 0 && pt.x < width && pt.y < width).then(|| (pt.x as usize, pt.y as usize))
+    (pt.x >= 0 && pt.y >= 0 && pt.x < width && pt.y < width).then(|| (pt.x as usize, pt.y as usize))
 }
 
 /*
@@ -240,6 +240,7 @@ impl MatrixOperations for HamiltonianObject {
 
                 let kinetic = sum; // * (-HBAR / ELECTRON_MASS / 2.0 / self.cfg.dx.powi(2));
 
+                //output[center_grid_coord] = kinetic; //kinetic + self.potential[center_grid_coord];
                 output[center_grid_coord] = kinetic; //kinetic + self.potential[center_grid_coord];
             }
         }
