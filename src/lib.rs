8ba6565d1b009597236c13f7163833505126c97b
Duncan Freeman <duncan.freeman1@gmail.com>
2024-01-20 17:19:42 -0800

    Begin fleshing out sim code

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+mod sim;
+mod array2d;
+
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
 #[derive(serde::Deserialize, serde::Serialize)]
 #[serde(default)] // if we add new fields, give them default values when deserializing old state
